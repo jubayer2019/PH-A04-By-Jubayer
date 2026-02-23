f9930fdd59d6eafacfaad22f4c5497cba9e9a889
jubayer2019 <jubayer.prodesigner@gmail.com>
2026-02-22 19:23:19 -0500

    Script added

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let InterviewFilterBtn = document.getElementById('interview-filter-btn');
 let RejectedFilterBtn = document.getElementById('rejected-filter-btn');
 
 const jobCards= document.getElementById('jobCards');
+const filteredSection = document.getElementById('filteredSection');
 
 // Deligating event listener to jobCards container
 const jobCardsContainer = document.querySelector('#jobCards');
@@ -44,5 +45,55 @@ function toggleStyle(id){
 }
 
 jobCardsContainer.addEventListener('click', function(event){
-    const parentNode = event.target.parentNode.parentNode;
+    if(event.target.classList.contains('interview-filter-btn')){
+        const parentNode = event.target.parentNode.parentNode;
+        const jobName = parentNode.querySelector('.jobName').innerText;
+        const jobTitle = parentNode.querySelector('.jobTitle').innerText;
+        const jobType = parentNode.querySelector('.jobType').innerText;
+        const status = parentNode.querySelector('.status').innerText;
+        const jobDescription = parentNode.querySelector('.jobDescription').innerText;
+
+        const jobData = {
+            jobName,
+            jobTitle,
+            jobType,
+            status,
+            jobDescription
+        };
+    const existingJob = InterviewList.find(item=> item.jobName == jobData.jobName);
+    if(!existingJob){
+        InterviewList.push(jobData);
+        // calculateCount();
+    }
+    }
+console.log(InterviewList);
+
 })
+
+function renderInterviewList(){
+    filteredSection.innerHTML = '';
+    for(let interview of InterviewList){
+        let div = document.createElement('div');
+        div.classList=' bg-white rounded-md border-2 border-[#F1F2F4] p-5 my-4 md:flex md:justify-between';
+        div.innerHTML=`
+        <div class="card-content">
+            <div class="text-content">
+                <h2 class="text-[#002C5C] font-semibold text-[18px] mb-1 jobName">Mobile First Corp</h2>
+                <p class="mb-2 text-[#64748B] text-[16px] jobTitle">React Native Developer</p>
+                <p class="mb-4 text-[#64748B] text-[16px] jobType">Remote    •    Full-time    •    $130,000 - $175,000</p>
+                <span class="bg-[#EEF4FF] py-2 px-3 uppercase text-[14px] status">Not Applied</span>
+                <p class="mt-3 text-[#64748B] text-[16px] mb-4 jobDescription">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            </div>
+            <div class="btn flex gap-2">
+                <button class="border-2 border-[#10B981] py-1 px-3 uppercase text-[#10B981] rounded-sm">interview</button>
+                <button class="border-2 border-[#EF4444] py-1 px-3 uppercase text-[#EF4444] rounded-sm">Rejected</button>
+
+            </div>
+        </div>
+        <div class="card-bin mt-[30px] md:mt-0">
+            <button class="border-2 border-[#F1F2F4] p-2 rounded-full"><img src="./img/bin.png" alt=""></button>
+        </div>
+        `;
+        filteredSection.appendChild(div);
+    }
+}
